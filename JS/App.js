e1bc92f014c93fa2f26c7772c7ebe3ab0857ce07
LuvuyoBhele <113292877+LuvuyoBhele@users.noreply.github.com>
2025-12-22 22:50:25 +0200

    Add hover effects for cards and integrate Bootstrap styles

--- JS/App.js
+++ JS/App.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     console.log('App.js loaded');
-    if (window.jQuery) console.log('jQuery OK', $.fn.jquery);
-    else console.log('jQuery NOT FOUND');
-
-    $(".card").hover(
-      function(){ $(this).css("background-color", "yellow"); },
-      function(){ $(this).css("background-color", "pink"); }
-    );
 });
+
+$('.card').hover(
+  function(){
+  $(this).addClass('onHover')
+    console.log('hovered');
+},
+  function(){
+    $(this).removeClass('onHover')}
+);

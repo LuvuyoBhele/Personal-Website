18892902b85449ae5601d6845ab419fc11999f58
LuvuyoBhele <113292877+LuvuyoBhele@users.noreply.github.com>
2025-12-24 19:40:43 +0200

    Fix button press color and enable links on App page

--- JS/App.js
+++ JS/App.js
@@ -2,11 +2,27 @@ $(document).ready(function(){
     console.log('App.js loaded');
 });
 
-$('.card').hover(
+$('.card').mouseover(
   function(){
   $(this).addClass('onHover')
     console.log('hovered');
-},
+})
+
+$('.card').click(
+  function(){
+  $(this).addClass('onHover')
+    console.log('hovered');
+})
+
+$('.card').mouseout(
   function(){
     $(this).removeClass('onHover')}
 );
+
+document.getElementById('card-1').addEventListener('click', function() {
+  window.location.href = 'https://github.com/LuvuyoBhele/bill-please';
+});
+
+document.getElementById('card-2').addEventListener('click', function() {
+  window.location.href = 'https://github.com/LuvuyoBhele/Breaking-an-encryption';
+});
